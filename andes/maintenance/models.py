c697a6f9da20a8c5648347a51306f30e64e99145
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-01-31 14:46:02 -0300

    Little changes and additions

--- andes/maintenance/models.py
+++ andes/maintenance/models.py
@@ -1,18 +1,25 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 from datetime import date
 
 # Create your models here.
 
 class Family(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Subfamily(models.Model):
     name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.name
 
 class Airport(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Inventory(models.Model):
     machine_number = models.IntegerField(default=0)
@@ -23,11 +30,22 @@ class Inventory(models.Model):
     down_date = models.DateField(null=True)
     airport = models.ForeignKey(Airport, on_delete=models.PROTECT)
     annex = models.FileField(null=True)
+    def __str__(self):
+        return self.machine_number
 
 class Status(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class History(models.Model):
     machine_number = models.IntegerField(default=0)
     date = models.DateField(default=date.today)
     status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    def __str__(self):
+        return self.machine_number
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
