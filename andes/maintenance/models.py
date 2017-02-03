0dbded605b784e4befd6f678d421a19f2ee7aeb6
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-03 11:50:23 -0300

    More models including some changes

--- andes/maintenance/models.py
+++ andes/maintenance/models.py
@@ -4,39 +4,55 @@ from django.db import models
 from django.contrib.auth.models import User
 #from django.db.models.signals import post_save
 #from django.dispatch import receiver
-from datetime import date
+#from datetime import date
 
 # Create your models here.
 
 class Family(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    code = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=50)
     def __str__(self):
-        return self.name
+        return self.code
 
 class Subfamily(models.Model):
+    code = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=50)
+    def __str__(self):
+        return self.code
+
+class Category(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     def __str__(self):
         return self.name
 
 class Airport(models.Model):
-    name = models.CharField(max_length=100, primary_key=True)
+    code = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=100)
     def __str__(self):
-        return self.name
+        return self.code
 
 class Machine(models.Model):
     machine_number = models.PositiveIntegerField(primary_key=True)
-    airport = models.ForeignKey(Airport, on_delete=models.PROTECT)
-    hour_base = models.PositiveIntegerField()
-    hour_now = models.PositiveIntegerField()
-    km_base = models.PositiveIntegerField()
-    km_now = models.PositiveIntegerField()
+    family = models.ForeignKey(Family, on_delete=models.PROTECT)
+    subfamily = models.ForeignKey(Subfamily, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    hourmeter_base = models.PositiveIntegerField()
+    odometer_base = models.PositiveIntegerField()
     def __str__(self):
         return str(self.machine_number)
 
+class Hourmeter(models.Model):
+    machine_number = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    datetime = models.DateTimeField()
+    hourmeter_to_date = models.PositiveIntegerField()
+
+class Odometer(models.Model):
+    machine_number = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    datetime = models.DateTimeField()
+    odometer_to_date = models.PositiveIntegerField()
+
 class Inventory(models.Model):
     machine_number = models.ForeignKey(Machine, on_delete=models.PROTECT)
-    family = models.ForeignKey(Family, on_delete=models.PROTECT)
-    subfamily = models.ForeignKey(Subfamily, on_delete=models.PROTECT)
     purchase_value = models.DecimalField(max_digits=9, decimal_places=2)
     up_date = models.DateField()
     down_date = models.DateField(null=True, blank=True)
@@ -77,13 +93,6 @@ class Profile(models.Model):
     #print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
     #instance.profile.save()
 
-class History(models.Model):
-    machine_number = models.ForeignKey(Machine, on_delete=models.PROTECT)
-    date = models.DateField(default=date.today)
-    status = models.ForeignKey(Status, on_delete=models.PROTECT)
-    def __str__(self):
-        return '%s | %s' % (self.machine_number, self.date)
-
 class WorkType(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
     def __str__(self):
@@ -91,10 +100,19 @@ class WorkType(models.Model):
 
 class WorkOrder(models.Model):
     work_type = models.ForeignKey(WorkType, on_delete=models.PROTECT)
-    date = models.DateField(default=date.today)
-    mechanic = models.ForeignKey(User, on_delete=models.PROTECT)
+    datetime = models.DateTimeField()
+    mechanic = models.ForeignKey(User, on_delete=models.PROTECT, related_name='mechanic')
+    team_leader = models.ForeignKey(User, on_delete=models.PROTECT, related_name='team_leader')
     def __str(self):
         return str(self.work_type)
+
+class History(models.Model):
+    machine_number = models.ForeignKey(Machine, on_delete=models.PROTECT)
+    datetime = models.DateTimeField()
+    status = models.ForeignKey(Status, on_delete=models.PROTECT)
+    work_order = models.ForeignKey(WorkOrder, on_delete=models.PROTECT)
+    def __str__(self):
+        return '%s | %s' % (self.machine_number, self.date)
 
 class SparePart(models.Model):
     name = models.CharField(max_length=50, primary_key=True)
@@ -103,7 +121,7 @@ class SparePart(models.Model):
 
 class SparePartHistoricPrice(models.Model):
     spare_part = models.ForeignKey(SparePart, on_delete=models.PROTECT)
-    date = models.DateField(default=date.today)
+    date = models.DateField()
     price = models.DecimalField(max_digits=9, decimal_places=2)
     def __str__(self):
         return str(self.spare_part)
