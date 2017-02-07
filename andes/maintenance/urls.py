1c50070fa08a56eeba4001dc2cbc2d1c77b2475a
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-07 16:30:22 -0300

    More progress in the development of the forms

--- andes/maintenance/urls.py
+++ andes/maintenance/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<machine_number>[0-9]+)/pauta/$', views.pauta, name="pauta")
+    url(r'^(?P<machine_number>[0-9]+)/(?P<level>[0-9]+)/pauta/$', views.pauta, name="pauta")
 ]
--- andes/maintenance/views.py
+++ andes/maintenance/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 
-from .models import Machine
+from .models import Machine, MachineSparePart, MachineInput, MachineInstruction
 
 # Create your views here.
 def index(request):
@@ -9,9 +9,13 @@ def index(request):
     context = {'machine_list': machine_list}
     return render(request, 'maintenance/index.html', context)
 
-def pauta(request, machine_number):
+def pauta(request, machine_number, level):
     try:
         machine = Machine.objects.get(pk=machine_number)
     except Machine.DoesNotExist:
         raise Http404("Machine does not exist")
-    return render(request, 'maintenance/pauta.html', {'machine': machine})
+    machine_spare_part_list = MachineSparePart.objects.filter(machine_number=machine_number, level=level)
+    machine_input_list = MachineInput.objects.filter(machine_number=machine_number, level=level)
+    machine_instruction_list = MachineInstruction.objects.filter(machine_number=machine_number, level=level)
+    context = {'machine': machine, 'machine_spare_part_list': machine_spare_part_list, 'machine_input_list': machine_input_list, 'machine_instruction_list': machine_instruction_list}
+    return render(request, 'maintenance/pauta.html', context)
