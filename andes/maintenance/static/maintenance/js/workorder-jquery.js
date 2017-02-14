c93a50ef080553f3890b08a725fcdf34ad6563ce
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-14 12:42:26 -0300

    Refining the work order template

--- andes/maintenance/static/maintenance/js/workorder-jquery.js
+++ andes/maintenance/static/maintenance/js/workorder-jquery.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
     $.get('/sparepartlist/' + suffix, {}, function (data) {
       $('tr.spare_part_row:last').after( data );
     });
+    $("#spare_part_number_" + suffix).attr("required", "required");
   });
   $("#spare_part_del_row").on("click", function () {
     var suffix = $("tr.spare_part_row:last td:first select").attr("name").match(/\d+/);
     if ( suffix > 1 ) {
       $("tr.spare_part_row:last").remove();
+      $("#spare_part_number_" + (parseInt(suffix)-1).toString()).removeAttr("required");
     };
   });
 });
