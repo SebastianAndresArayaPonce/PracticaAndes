122eedcb1aba0ea3d5aca8c36a6c6ce3e73f1428
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-16 09:58:25 -0300

    Now i can generate a pdf. Next step is do the same with the dynamically generated workorder template

--- andes/maintenance/urls.py
+++ andes/maintenance/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import url
+from wkhtmltopdf.views import PDFTemplateView
 
 from . import views
 
 app_name = 'maintenance'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^pdf/$', PDFTemplateView.as_view(template_name='maintenance/index.html', filename='my_pdf.pdf'), name='pdf'),
     url(r'^guideline/machine/(?P<machine_number>[0-9]+)/level/(?P<level>[0-9]+)/$', views.guideline, name="guideline"),
     url(r'^workorder/(?P<workorder_number>[0-9]+)$', views.workorder, name="workorder"),
     url(r'^workorder/(?P<workorder_number>[0-9]+)/confirm$', views.confirm_workorder, name="confirm_workorder"),
--- andes/mysite/settings.py
+++ andes/mysite/settings.py
@@ -120,4 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'maintenance/static/')
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'maintenance/media/')
+MEDIA_URL = '/media/'
