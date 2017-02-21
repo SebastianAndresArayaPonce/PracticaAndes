9537503245d4c6e2a7afaf4c008987737d171248
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-20 22:27:34 -0300

    Trying to make wkhtmltopdf process header & footer. Problem with qt

--- andes/maintenance/views.py
+++ andes/maintenance/views.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
@@ -33,24 +35,28 @@ def workorder(request, workorder_number):
         workorder = WorkOrder.objects.get(pk=workorder_number)
     except WorkOrder.DoesNotExist:
         raise Http404("WorkOrder does not exist")
-    airport = Airport.objects.get(pk=Inventory.objects.filter(machine_number=workorder.machine_number.machine_number).latest('up_date').airport)
-    inputs = Input.objects.all()
-    spare_parts = SparePart.objects.all()
-    template = 'maintenance/workorder.html'
-    context = {'workorder': workorder, 'airport': airport, 'inputs': inputs, 'spare_parts': spare_parts}
+    context = {}
+    context['workorder'] = workorder
+    context['airport'] = Airport.objects.get(pk=Inventory.objects.filter(machine_number=workorder.machine_number.machine_number).latest('up_date').airport)
+    context['inputs'] = Input.objects.all()
+    context['spare_parts'] = SparePart.objects.all()
+
     if workorder.work_type.name == 'Preventivo':
         context['machine_spare_part_list'] = MachineSparePart.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
         context['machine_input_list'] = MachineInput.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
+
         machine_instruction_list = MachineInstruction.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
         context['first_instruction_list'] = machine_instruction_list.filter(instruction_type="First")
         machine_instruction_list = machine_instruction_list.exclude(instruction_type="First")
         context['last_instruction_list'] = machine_instruction_list.filter(instruction_type="Last")
         machine_instruction_list = machine_instruction_list.exclude(instruction_type="Last")
         context['machine_instruction_list'] = machine_instruction_list
+
         instruction_types = set()
         for i in machine_instruction_list:
             instruction_types.add(i.instruction_type.name)
         context['instruction_types'] = list(instruction_types)
+
         machine_lubrication_chart_list = MachineLubricationChart.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
         context['machine_lubrication_chart_list'] = machine_lubrication_chart_list
         lubrication_chart_descriptions = []
@@ -58,7 +64,11 @@ def workorder(request, workorder_number):
             for j in LubricationChartDescription.objects.filter(lubrication_chart=i.id):
                 lubrication_chart_descriptions.append((j.number, j.description))
         context['lubrication_chart_descriptions'] = sorted(lubrication_chart_descriptions)
+
         context['today'] = datetime.today()
+
+    template = 'maintenance/workorder.html'
+
     return render(request, template, context)
 
 @login_required
@@ -68,16 +78,19 @@ def process_workorder(request, workorder_number):
     except WorkOrder.DoesNotExist:
         raise Http404("WorkOrder does not exist")
 
-    airport_code = (Airport.objects.get(pk=Inventory.objects.filter(machine_number=workorder.machine_number.machine_number).latest('up_date').airport)).code
+    context = {}
+    context['workorder'] = workorder
+    context['airport_code'] = (Airport.objects.get(pk=Inventory.objects.filter(machine_number=workorder.machine_number.machine_number).latest('up_date').airport)).code
     form = request.POST
 
     out = map(int, form['output_date'].split("-") + form['output_time'].split(":"))
     out_datetime = datetime(out[0], out[1], out[2], out[3], out[4])
+    context['out_datetime'] = out_datetime
 
     w_entries = dict((s,form[s]) for s in form.keys() if "work_description" in s and form[s] != "")
     w_order = sorted(w_entries)
     is_w_len_greater_than_0 = len(w_entries) > 0
-    work_descriptions = {'entries': w_entries, 'order': w_order, 'validator': is_w_len_greater_than_0}
+    context['work_descriptions'] = {'entries': w_entries, 'order': w_order, 'validator': is_w_len_greater_than_0}
 
     sp_ids = dict((s,form[s]) for s in form.keys() if "spare_part_number" in s and form[s] != "")
     sp_numbers = {}
@@ -97,7 +110,7 @@ def process_workorder(request, workorder_number):
         range_sp.append((sp_order_numbers[len(sp_ids)-1], sp_order_descriptions[len(sp_ids)-1], sp_order_quantitys[len(sp_ids)-1], "", "", ""))
     else:
         range_sp = [(sp_order_numbers[i], sp_order_descriptions[i], sp_order_quantitys[i], sp_order_numbers[i+1], sp_order_descriptions[i+1], sp_order_quantitys[i+1]) for i in xrange(0, len(sp_ids), 2)]
-    spare_parts = {'numbers': sp_numbers, 'descriptions': sp_descriptions, 'quantitys': sp_quantitys, 'range': range_sp, 'validator': is_sp_len_greater_than_0}
+    context['spare_parts'] = {'numbers': sp_numbers, 'descriptions': sp_descriptions, 'quantitys': sp_quantitys, 'range': range_sp, 'validator': is_sp_len_greater_than_0}
 
     i_ids = dict((s,form[s]) for s in form.keys() if "input_description" in s and form[s] != "")
     i_types = {}
@@ -108,14 +121,49 @@ def process_workorder(request, workorder_number):
     i_order_quantitys = sorted(i_quantitys)
     is_i_len_greater_than_0 = len(i_ids)
     range_i = [(i_order_types[i],i_order_quantitys[i]) for i in xrange(len(i_ids))]
-    inputs = {'types': i_types, 'quantitys': i_quantitys, 'range': range_i, 'validator': is_i_len_greater_than_0}
+    context['inputs'] = {'types': i_types, 'quantitys': i_quantitys, 'range': range_i, 'validator': is_i_len_greater_than_0}
 
-    template = 'maintenance/process_workorder.html'
+    if workorder.work_type.name == 'Preventivo':
+        context['machine_spare_part_list'] = MachineSparePart.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
+        context['machine_input_list'] = MachineInput.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
+
+        machine_instruction_list = MachineInstruction.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
+        context['first_instruction_list'] = machine_instruction_list.filter(instruction_type="First")
+        machine_instruction_list = machine_instruction_list.exclude(instruction_type="First")
+        context['last_instruction_list'] = machine_instruction_list.filter(instruction_type="Last")
+        machine_instruction_list = machine_instruction_list.exclude(instruction_type="Last")
+        context['machine_instruction_list'] = machine_instruction_list
+
+        instruction_types = set()
+        for i in machine_instruction_list:
+            instruction_types.add(i.instruction_type.name)
+        context['instruction_types'] = list(instruction_types)
+
+        machine_lubrication_chart_list = MachineLubricationChart.objects.filter(machine_number=workorder.machine_number, level=workorder.level)
+        context['machine_lubrication_chart_list'] = machine_lubrication_chart_list
+        lc_descriptions = []
+        for i in machine_lubrication_chart_list:
+            for j in LubricationChartDescription.objects.filter(lubrication_chart=i.id):
+                lc_descriptions.append((j.number, j.description))
+        len_lcd = len(lc_descriptions)
+        lc_descriptions = sorted(lc_descriptions)
+        if len_lcd%2 == 1:
+            range_lcd = [(lc_descriptions[i][0],lc_descriptions[i][1], lc_descriptions[(len_lcd/2)+i+1][0], lc_descriptions[(len_lcd/2)+i+1][1]) for i in xrange(0, len_lcd/2)]
+            range_lcd.append((lc_descriptions[len_lcd/2][0], lc_descriptions[len_lcd/2][1], "", ""))
+        else:
+            range_lcd = [(lc_descriptions[i][0],lc_descriptions[i][1], lc_descriptions[(len_lcd/2)+i][0], lc_descriptions[(len_lcd/2)+i][1]) for i in xrange(0, len_lcd/2)]
+        context['lubrication_chart_descriptions'] = range_lcd
+
+        context['today'] = datetime.today()
+
+    #template = 'maintenance/process_workorder.html'
+    template = 'maintenance/guideline.html'
+    header = 'maintenance/guideline_header.html'
+    cmd_options = {'footer-right': 'Pagina [page] de [topage]'}
     filename = str(workorder.machine_number.machine_number) + " " + str(out_datetime)
-    context = {'workorder': workorder, 'airport_code': airport_code, 'out_datetime': out_datetime, 'work_descriptions': work_descriptions, 'spare_parts': spare_parts, 'inputs': inputs}
 
-    #return render(request, template, context)
-    return PDFTemplateResponse(request=request, template=template, filename=filename, context=context, show_content_in_browser=True)
+    #return PDFTemplateResponse(request=request, template=template, filename=filename, context=context, show_content_in_browser=True)
+    return PDFTemplateResponse(request=request, template=template, header_template=header, filename=filename, context=context, show_content_in_browser=True, cmd_options=cmd_options)
 
 @login_required
 def get_work_description(request, suffix):
