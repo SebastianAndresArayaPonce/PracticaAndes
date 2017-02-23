196dca47899f7ad37199ebde61e907a9af7f232d
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-23 09:34:59 -0300

    Now the images are saved where they are supposed to be

--- andes/maintenance/models.py
+++ andes/maintenance/models.py
@@ -125,7 +125,7 @@ class MachineInstruction(models.Model):
 
 class LubricationChart(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField()
+    image = models.ImageField(upload_to="images/")
     def __unicode__(self):
         return self.name
 
