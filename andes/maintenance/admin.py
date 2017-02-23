7a379fdc4051a496e706774ea073b0c13204c026
Cristian Eduardo Araya Urzúa <caraya@alumnos.inf.utfsm.cl>
2017-02-23 08:43:26 -0300

    Changing template of user creation in the admin page

--- andes/maintenance/admin.py
+++ andes/maintenance/admin.py
@@ -4,6 +4,7 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.forms import UserCreationForm as BaseUserCreationForm
 from django.contrib.auth.forms import UserChangeForm as BaseUserChangeForm
 from django.contrib.auth.models import User
+from django.forms.models import BaseInlineFormSet
 
 from .models import *
 
@@ -12,11 +13,28 @@ from .models import *
 class MachineInstructionAdmin(admin.ModelAdmin):
     fields = ['machine_number', 'instruction_number', 'level']
 
+class UserProfileInlineFormSet(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            try:
+                if form.cleaned_data:
+                    count += 1
+            except AttributeError:
+                pass
+        if count == 0:
+            raise ValidationError('This fields are required.')
+
 class UserProfileInline(admin.StackedInline):
     model = Profile
+    extra = 1
+    max_num = 1
     can_delete = False
     verbose_name_plural = 'Profile'
     fk_name = 'user'
+    template = 'admin/maintenance/stacked.html'
+    formset = UserProfileInlineFormSet
+    formset_required = True
 
 class UserCreationForm(BaseUserCreationForm):
     class Meta:
@@ -25,23 +43,23 @@ class UserCreationForm(BaseUserCreationForm):
 
     def clean_first_name(self):
         if self.cleaned_data["first_name"].strip() == '':
-            raise ValidationError("First name is required.")
+            raise ValidationError("This field is required.")
         return self.cleaned_data["first_name"]
 
     def clean_last_name(self):
         if self.cleaned_data["last_name"].strip() == '':
-            raise ValidationError("Last name is required.")
+            raise ValidationError("This field is required.")
         return self.cleaned_data["last_name"]
 
 class UserChangeForm(BaseUserChangeForm):
     def clean_first_name(self):
         if self.cleaned_data["first_name"].strip() == '':
-            raise ValidationError("First name is required.")
+            raise ValidationError("This field is required.")
         return self.cleaned_data["first_name"]
 
     def clean_last_name(self):
         if self.cleaned_data["last_name"].strip() == '':
-            raise ValidationError("Last name is required.")
+            raise ValidationError("This field is required.")
         return self.cleaned_data["last_name"]
 
 class UserAdmin(BaseUserAdmin):
@@ -73,10 +91,10 @@ admin.site.register(MachineLubricationChart)
 admin.site.register(WorkType)
 admin.site.register(WorkOrder)
 admin.site.register(Airport)
+admin.site.register(Area)
 admin.site.register(Inventory)
 #admin.site.register(Status)
 #admin.site.register(History)
-#admin.site.register(Area)
 #admin.site.register(PurchaseOrder)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
